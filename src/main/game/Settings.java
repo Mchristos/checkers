a48dfaf0b5f93a6a2c99da96e903ab763a1f5a4a
chris <chris.the.marais@gmail.com>
2018-05-01 22:25:49 +0100

    remove unused setting

--- src/main/game/Settings.java
+++ src/main/game/Settings.java
@@ -1,7 +1,6 @@
 package main.game;
 
 public class Settings {
-    public static boolean TWOPLAYER = false; // by default, human plays AI
     public static boolean FORCETAKES = true; //
     public static Player FIRSTMOVE = Player.HUMAN; // who moves first
     public static int AI_DEPTH = 7;
