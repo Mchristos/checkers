a42013fd4ae16af02799f67db30c6515f34b3ac2
chris <chris.the.marais@gmail.com>
2018-05-01 13:14:42 +0100

    docs for AI

--- src/main/game/AI.java
+++ src/main/game/AI.java
@@ -5,7 +5,9 @@ import java.util.Random;
 
 public class AI {
 
+    // determines the depth that the AI searches to in minimax
     private int depth;
+    // which player the AI searches with respect to
     private Player player;
 
     public AI(){
@@ -19,15 +21,20 @@ public class AI {
     }
 
     public BoardState move(BoardState state, Player player){
-        if (state.getTurn()==player){
+        if (state.getTurn() == player){
             ArrayList<BoardState> successors = state.getSuccessors();
             return minimaxMove(successors);
         }
         else{
-            throw new RuntimeException("Invalid player");
+            throw new RuntimeException("Cannot generate moves for player if it's not their turn");
         }
     }
 
+    /**
+     * Chooses best successor state based on the minimax algorithm.
+     * @param successors
+     * @return
+     */
     private BoardState minimaxMove(ArrayList<BoardState> successors){
         int bestScore = Integer.MIN_VALUE;
         ArrayList<BoardState> equalBests = new ArrayList<>();
@@ -44,9 +51,15 @@ public class AI {
         if(equalBests.size() > 1){
             System.out.println(player.toString() + " choosing random best move");
         }
+        // choose randomly from equally scoring best moves
         return randomMove(equalBests);
-
     }
+
+    /**
+     * Chooses a successor state randomly.
+     * @param successors
+     * @return
+     */
     private BoardState randomMove(ArrayList<BoardState> successors){
         if (successors.size() < 1){
             throw new RuntimeException("Can't randomly choose from empty list.");
@@ -64,36 +77,48 @@ public class AI {
      * @return minimax score associated with node
      */
     private int minimax(BoardState node, int depth){
-        int alpha = Integer.MIN_VALUE; // alpha computed as a MAX
-        int beta = Integer.MAX_VALUE; // beta computes as a MIN
+        // initialize alpha (computed as a max)
+        int alpha = Integer.MIN_VALUE;
+        // initialize beta (computed as a min)
+        int beta = Integer.MAX_VALUE;
+        // call minimax
         return minimax(node, depth, alpha, beta);
     }
 
+    /**
+     * Implements the minimax algorithm with alpha-beta pruning
+     * @param node
+     * @param depth
+     * @param alpha
+     * @param beta
+     * @return
+     */
     private int minimax(BoardState node, int depth, int alpha, int beta){
         if (depth == 0 || node.isGameOver()){
-//            if(node.pieceCount.get(player.getOpposite()) == 0 ) {
-//                System.out.println(player.toString() + " can see a winning sequence");
-//            }
             return node.computeHeuristic(this.player);
         }
-        if (node.getTurn() == player){ // MAX
-            // AI tries to maximize this value
+        // MAX player = player
+        if (node.getTurn() == player){
+            // player tries to maximize this value
             int v = Integer.MIN_VALUE;
             for (BoardState child : node.getSuccessors()){
                 v = Math.max(v, minimax(child, depth-1, alpha, beta));
                 alpha = Math.max(alpha, v);
+                // prune
                 if (alpha >= beta){
                     break;
                 }
             }
             return v;
         }
-        if (node.getTurn() == player.getOpposite()){ // MIN
-            // human tries to minimize this value
+        // MIN player = opponent
+        if (node.getTurn() == player.getOpposite()){
+            // opponent tries to minimize this value
             int v = Integer.MAX_VALUE;
             for (BoardState child : node.getSuccessors()){
                 v = Math.min(v,minimax(child, depth-1, alpha, beta));
                 beta = Math.min(beta, v);
+                // prune
                 if (alpha >= beta){
                     break;
                 }
