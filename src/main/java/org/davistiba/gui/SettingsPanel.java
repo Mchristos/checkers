d5045d67f7175a3e190ecc94b76b100c2d04f568
Davis Tibbz <davisinyo@gmail.com>
2024-11-23 13:11:23 +0300

    make constants final inside SettingsPanel

--- src/main/java/org/davistiba/gui/SettingsPanel.java
+++ src/main/java/org/davistiba/gui/SettingsPanel.java
@@ -8,12 +8,12 @@ public class SettingsPanel {
     public static boolean helpMode = true;
     public static boolean hintMode = false;
     public static boolean dragDrop = false;
-    public static int AiMinPauseDurationInMs = 800;
-    public static int squareSize = 80;
-    public static int checkerWidth = 5 * squareSize / 6;
-    public static int checkerHeight = 5 * squareSize / 6;
-    public static int ghostButtonWidth = 30 * squareSize / 29;
-    public static int ghostButtonHeight = 5 * squareSize / 6;
+    public static final int AiMinPauseDurationInMs = 800;
+    public static final int squareSize = 80;
+    public static final int checkerWidth = 5 * squareSize / 6;
+    public static final int checkerHeight = 5 * squareSize / 6;
+    public static final int ghostButtonWidth = 30 * squareSize / 29;
+    public static final int ghostButtonHeight = 5 * squareSize / 6;
 
     /**
      * Gets the correct colour (black/white) for the given player
